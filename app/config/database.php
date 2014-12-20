634250c4aefc01bf4ade7f273888b7e29773dbe0
Jhonatan Osorio <jaosorio1013@gmail.com>
2014-12-19 19:20:49 -0500

    Organizando la seguridad

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'scrum_cocina',
-			'username'  => 'root',
-			'password'  => 'gi33',
+			'host'      => getenv('DB_HOSOT'),
+			'database'  => getenv('DB_NAME'),
+			'username'  => getenv('DB_USERNAME'),
+			'password'  => getenv('DB_PASSWORD'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
