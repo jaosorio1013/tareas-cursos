e53fb22d4e29da1c04c904409fa5871d3fa44ddf
Jhonatan Osorio <jaosorio1013@gmail.com>
2014-12-30 14:01:40 -0500

    Organiza las vistas para asignar tareas

---
+++ app/views/cursos/asignar-tareas.blade.php
@@ -0,0 +1,35 @@
+@extends('layout')
+
+
+@section('content')
+    <div class="container">
+        <h1>Asignar tareas al Curso ({{ $curso->nombre }})</h1>
+
+        {{ Form::open(['route' => ['asignar_tareas_sprint', $curso->id], 'method' => 'PUT', 'role' => 'form', 'novalidate']) }}
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>*</th>
+                <th>Prioridad</th>
+                <th>Titulo</th>
+                <th>Curso</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($tareas AS $tarea)
+                <tr>
+                    <td>{{ Form::checkbox('tarea_id[]', $tarea->id) }}</td>
+                    <td>{{ $tarea->prioridad }}</td>
+                    <td><a href="{{ route('actualizar_tarea', [$tarea->id]) }}">{{ $tarea->titulo }}</a></td>
+                    <td>{{ $tarea->curso->nombre }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+
+        <div class="form-group">
+            <input type="submit" class="btn btn-success" value="Crear tarea"/>
+        </div>
+        {{ Form::close() }}
+    </div>
+@stop
